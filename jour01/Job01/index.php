b6bb914e418d5ccff99bc1391b2a114d71ebc8cc
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-16 14:07:28 +0200

    Add files via upload

---
+++ jour01/Job01/index.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Job01";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01</title>
+</head>
+<body>
+    <?php 
+    $str="LaPlateforme";
+    $str2="Vive ";
+    $str3="!"; ?>
+    <?php
+    echo $str2;
+    echo $str;
+    echo $str3; ?>
+</body>
+</html>
