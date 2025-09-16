cc0a5a2a5c780a080da5647be31311f1ca929fb4
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-16 14:48:36 +0200

    ajout de commits

---
+++ jour01/Job02/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Job02";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job02</title>
+</head>
+<body>
+  <p><?php echo "Hello LaPlateforme!"; ?></p>
+  <?php echo "Hello LaPlateforme!"; ?>
+</body>
+</html>
