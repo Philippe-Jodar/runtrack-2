394b46ca03cda0e752c31d6ae4d936f35dafc1d0
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-19 13:52:22 +0200

    Modifications de commits

---
+++ jour02/Job01/index.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Job01";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01</title>
+</head>
+<body>
+<?php
+/**Afficher tous les nombres compris entre 0 et 1337 en mettant un retour à la ligne entre
+chaque nombre (<br />).
+Le nombre 42 doit être en gras et souligné (<b><u>...).
+*/
+for ($x= 1; $x< 1337; $x++)
+  {if ($x== 1337)
+  {break;}
+  if ($x==42) echo "<b><u>42</b></u>";
+  else echo $x;
+  echo '<br/>';
+}
+?>
+</body>
+</html>
