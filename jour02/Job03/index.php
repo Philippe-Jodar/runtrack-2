1d379584a5999117cf3af492ecb353361d436cfb
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-24 10:09:52 +0200

    modifications de commits

--- jour02/Job03/index.php
+++ jour02/Job03/index.php
@@ -10,11 +10,6 @@ $title = "Job03";
 </head>
 <body>
 <?php 
-/**Afficher les nombres de 0 à 100 en mettant un retour à la ligne entre chaque nombre
-(<br />).
-Si le nombre est entre 0 et 20 : écrire en italique (<i>), si le nombre est compris entre 25
-et 50 : souligner.
-Afficher “La Plateforme_” à la place de 42. */
 for ($x= 0; $x< 101; $x++)
   if ($x == 42)
     {
