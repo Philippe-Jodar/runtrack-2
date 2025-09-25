e9d5b8a40c5b87826e803d5d11d38ee7573561c1
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-25 16:28:00 +0200

    modifs de ommits

--- jour04/Job02/index.php
+++ jour04/Job02/index.php
@@ -7,11 +7,9 @@ $title = "J4/Job02";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>J4-Job02</title>
-    
 </head>
 <body>
 <h2>Formulaire</h2>
-
     <form method="get" action="">
         <label>Nom : <input type="text" name="nom"></label><br><br>
         <label>Prénom : <input type="text" name="prenom"></label><br><br>
@@ -19,14 +17,12 @@ $title = "J4/Job02";
         <label>Ville : <input type="text" name="ville"></label><br><br>
         <input type="submit" value="Envoyer">
     </form>
-
     <?php
     // Vérifie s’il y a des paramètres GET
     if (!empty($_GET)) {
         echo "<h3>Paramètres reçus :</h3>";
         echo "<table border='1' cellpadding='5'>";
         echo "<tr><th>Argument</th><th>Valeur</th></tr>";
-
         // Parcours chaque argument GET
         foreach ($_GET as $argument => $valeur) {
             echo "<tr>";
@@ -34,10 +30,8 @@ $title = "J4/Job02";
             echo "<td>" . $valeur . "</td>";
             echo "</tr>";
         }
-
         echo "</table>";
     }
     ?>
-
 </body>
 </html>
