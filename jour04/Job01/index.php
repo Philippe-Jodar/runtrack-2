02f691f7a97051794c11e4d2b84ded4455c0ec20
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-25 16:30:28 +0200

    modification de comits

--- jour04/Job01/index.php
+++ jour04/Job01/index.php
@@ -1,12 +1,12 @@
 <?php
-$title = "Jour04/Job01";
+$title = "Jour04/Job02";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour04/Job01</title>
+    <title>Jour04/Job02</title>
     <style>
         input[type="text"] {
             margin-bottom: 10px;
