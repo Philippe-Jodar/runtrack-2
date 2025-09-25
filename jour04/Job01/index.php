9c60ab52d6ab115a25ec431dc5a607163bea0455
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-25 15:45:58 +0200

    Ajout de commits

---
+++ jour04/Job01/index.php
@@ -0,0 +1,33 @@
+<?php
+$title = "Jour04/Job01";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour04/Job01</title>
+</head>
+<body>
+<form method="GET" action="">
+        <label>Nom : <input type="text" name="Nom"></label><br>
+        <label>Prénom : <input type="text" name="Prenom"></label><br>
+        <label>Adresse : <input type="text" name="Adresse"></label><br>
+        <label>Email : <input type="text" name="Email"></label><br>
+        <input type="submit" value="Envoyer">
+    </form>
+    <?php
+    $arguments = 0;
+    if (!empty($_GET)) {
+        $arguments = count($_GET);
+        echo "<p>Nombre d'arguments GET reçus : $arguments</p>";
+        echo "<ul>";
+        foreach ($_GET as $cle => $valeur) {
+            echo "<li><strong>$cle</strong> : $valeur</li>";
+        }
+        echo "</ul>";
+    };
+    echo "Le nombre d'argument GET envoyé est : $arguments";
+    ?>
+</body>
+</html>
