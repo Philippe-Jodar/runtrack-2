2ffa567afefdcd4ca77cb8ab6f83ce6fcf6a3a9a
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-25 09:48:54 +0200

    Ajout de commits

---
+++ jour02/Job7/index.php
@@ -0,0 +1,32 @@
+<?php
+$title = "Job7";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$hauteur=5;
+$d=$hauteur-1;
+$c=1;
+for($x=0 ; $x<$hauteur ; $x++)
+{
+for($a=1 ; $a<=$d ; $a++)
+{
+echo "";
+}
+for($b=1 ; $b<=$c ; $b++)
+{
+echo "*";
+}
+echo "<br/>";
+$d--;
+$c+=2;
+} 
+?>
+</body>
+</html>
