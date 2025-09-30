bb72aff01ffa947d8dbf6fe8745d7bff4ec6bbfc
Philippe Jodar <philippe.jodar@laplateforme.io>
2025-09-30 14:53:11 +0200

    modifs

--- jour04/Job05/index.php
+++ jour04/Job05/index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>J4-Job06</title>
     <style>
         table {
             border-collapse: collapse;
